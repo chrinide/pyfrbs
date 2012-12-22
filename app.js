ccce515e13f8c6e9b50a6e3fe71168ae9724ec59
Ivan Zenin <i2n.z3n@gmail.com>
2012-12-22 08:04:11 +0400

    routes are moved to app.js

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ var express = require('express'),
   app = express(),
   mongoose = require('mongoose'),
   models = require('./models'),
-  routes = require('./routes'),
-  opus = require('./routes/opus'),
-  user = require('./routes/user'),
   http = require('http'),
   path = require('path'),
-  db, Opus;
+  Opus, db;
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -35,13 +32,39 @@ models.defineModels(mongoose, function() {
   db = mongoose.connect('mongodb://localhost/43');
 });
 
-app.get('/', routes.index);
-app.get('/opuses.:format', opus.list);
-app.post('/opuses.:format?', opus.add);
-app.get('/opuses/:id.:format?', opus.get);
-app.put('/opuses/:id.:format?', opus.edit);
-app.del('/opuses/:id.:format?', opus.del);
-app.get('/users', user.list);
+app.get('/', function(req, res) {
+	res.redirect('/opuses');
+});
+
+app.get('/opuses', function(req, res) {
+  mongoose.model('Opus').find(function(opuses) {
+	  opuses = opuses.map(function(o) {
+        return o.__opus;
+      });
+      res.render('opuses/index.jade', {
+		  locals: { opuses: opuses }
+	  });
+  });
+});
+
+app.post('/opuses.:format?', function(req, res) {
+  var opus = new Opus(req.body['opus']);
+  opus.save(function() {
+    switch (req.params.format) {
+      case 'json':
+        req.send(o.__opus);
+        break;
+      default:
+        res.redirect('/opuses');
+    }
+  });
+});
+
+app.get('/opuses/:id.:format?', function(req, res) {});
+
+app.put('/opuses/:id.:format?', function(req, res) {});
+
+app.del('/opuses/:id.:format?', function(req, res) {});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
