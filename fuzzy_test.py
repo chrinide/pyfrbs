8134caca2c13759c1c58a640997bc9771314be42
Иван Зенин <zenin@acti.ru>
2014-12-20 13:14:03 +0300

    Add some more tests

--- fuzzy_test.py
+++ fuzzy_test.py
@@ -9,28 +9,36 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtTest import QTest
 
 class tests(unittest.TestCase):
-    def test_variables_tab_initialization(self):
+    def test_variables(self):
         self.assertEqual(window.uiTabs.currentIndex(), 0)
         self.assertEqual(window.uiVariablesCombo.currentIndex(), -1)
         self.assertEqual(window.uiCreateVariableButton.isEnabled(), True)
         self.assertEqual(window.uiRenameVariableButton.isEnabled(), False)
         self.assertEqual(window.uiDeleteVariableButton.isEnabled(), False)
+    
+    def test_ranges(self):
         self.assertEqual(window.uiRangeMinEdit.isEnabled(), False)
         self.assertEqual(window.uiRangeMinEdit.text(), '')
         self.assertEqual(window.uiRangeMaxEdit.isEnabled(), False)
         self.assertEqual(window.uiRangeMaxEdit.text(), '')
+
+    def test_terms(self):
         self.assertEqual(window.uiTermsCombo.currentIndex(), -1)
         self.assertEqual(window.uiTermsCombo.isEnabled(), False)
         self.assertEqual(window.uiTermsList.count(), 0)
         self.assertEqual(window.uiTermsList.isEnabled(), False)
         self.assertEqual(window.uiAddTermButton.isEnabled(), False)
         self.assertEqual(window.uiRemoveTermButton.isEnabled(), False)
+
+    def test_hedges(self):
         self.assertEqual(window.uiHedgesCombo.currentIndex(), -1)
         self.assertEqual(window.uiHedgesCombo.isEnabled(), False)
         self.assertEqual(window.uiHedgesList.count(), 0)
         self.assertEqual(window.uiHedgesList.isEnabled(), False)
         self.assertEqual(window.uiAddHedgeButton.isEnabled(), False)
         self.assertEqual(window.uiRemoveHedgeButton.isEnabled(), False)
+
+    def test_commit(self):
         self.assertEqual(window.uiCommitVariableButton.isEnabled(), False)
 
 if __name__ == '__main__':
