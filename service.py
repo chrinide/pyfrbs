5c91b008ad0ff8ea0a57769f23bde0c1101f03d4
Иван Зенин <zenin@acti.ru>
2015-03-17 12:19:40 +0300

    Rename files, add service stuff

---
+++ service.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Hello, World!"
+
+app.run(debug=True)
+
