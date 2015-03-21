ddbd84d20be32856e21c8235ed0d036a7d730146
Ivan Zenin <i.zenin@gmx.com>
2015-03-21 20:30:33 +0300

    Implement variables API

--- service.py
+++ service.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
 from flask import Flask, jsonify, request, abort, g
-from werkzeug.exceptions import default_exceptions
-from werkzeug.exceptions import HTTPException
-import psycopg2
-import psycopg2.extras
-import sys
+from werkzeug.exceptions import default_exceptions, HTTPException
+import psycopg2, psycopg2.extras
+import argparse, sys
 
-# force error handlers to response with json instead of html
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, add_help=False)
+parser.add_argument('-a', dest='address', default='127.0.0.1:5432')
+parser.add_argument('-d', dest='database', default='fuzzy')
+parser.add_argument('-u', dest='username', default='user1')
+parser.add_argument('-p', dest='password', default='pass1')
+opts = parser.parse_args()
 
 def make_json_error(ex):
     response = jsonify(message=str(ex))
@@ -19,11 +22,10 @@ app = Flask(__name__)
 for code in default_exceptions.items():
     app.error_handler_spec[None][code[0]] = make_json_error
 
-# manage database connections 
-
 @app.before_request
 def before_request():
-    g.db = psycopg2.connect(host='127.0.0.1', database='fuzzy', user='user1', password='pass1', cursor_factory=psycopg2.extras.RealDictCursor)
+    g.db = psycopg2.connect(host=opts.address.split(':')[0], port=opts.address.split(':')[1], 
+            database=opts.database, user=opts.username, password=opts.password)
 
 @app.teardown_request
 def teardown_request(exception):
@@ -31,54 +33,95 @@ def teardown_request(exception):
     if db is not None:
         db.close()
 
-# request handlers
-
 @app.route('/api/variables', methods=['GET'])
 def get_variables():
-    cur = g.db.cursor()
-    cur.execute('SELECT id, name, name_id, validated, min, max FROM variables;')
-    res = cur.fetchall()
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute('SELECT id, name_id, name, validated, min, max FROM variables;')
+    variables = cur.fetchall()
     cur.close()
-    return jsonify({'variables': res})
+    return jsonify({'variables': variables})
 
 @app.route('/api/variables/<int:variable_id>', methods=['GET'])
 def get_variable(variable_id):
-    cur = g.db.cursor()
-    cur.execute('SELECT id, name, name_id, validated, min, max FROM variables WHERE id = %s;', (variable_id,))
-    res = cur.fetchall()
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute('SELECT TOP 1 id, name_id, name, validated, min, max FROM variables WHERE id = %s;', (variable_id,))
+    variable = cur.fetchone()
     cur.close()
-    if res:
-        return jsonify({'variable': res})
+    if variable:
+        return jsonify({'variable': variable})
     abort(404)
-
-# TODO: rework with real database
 
 @app.route('/api/variables', methods=['POST'])
 def create_variable():
-    variable = {
-        'id': variable[-1]['id'] + 1,
-        'name_id': request.json['name_id'],
-        'name': request.json['name'],
-        'validated': request.json['validated'],
-        'min': request.json['min'],
-        'max': request.json['max']
-    }
-    variables.append(variable)
-    return jsonify({'variable': variable}), 201, {'location': '/api/variables/' % variable['id']}
+    if not request.json:
+        abort(400)
+    # TODO: which fields are required?
+    if not 'name_id' in request.json or int(request.json['name_id']) <= 0 or not 'name' in request.json:
+        abort(400)
+    cur = g.db.cursor()
+    cur.execute('SELECT id FROM groups WHERE id = %s LIMIT 1;', (request.json['name_id'],))
+    if not cur.fetchone():
+        abort(400)
+    if not 'min' in request.json or not 'max' in request.json or float(request.json['min']) > float(request.json['max']):
+        abort(400)
+    cur.execute('INSERT INTO variables (name_id, name, validated, min, max) VALUES (%s, %s, %s, %s, %s) RETURNING id;', 
+            (request.json['name_id'], request.json['name'], False, request.json['min'], request.json['max']))
+    variable_id = cur.fetchone()
+    cur.close()
+    if variable_id:
+        g.db.commit()
+        return jsonify(), 201, {'location': '/api/variables/%d' % variable_id}
+    abort(500)
+
+@app.route('/api/variables/<int:variable_id>', methods=['PUT'])
+def update_variable(variable_id):
+    cur = g.db.cursor()
+    cur.execute('SELECT id FROM variables WHERE id = %s LIMIT 1;', (variable_id,))
+    if not cur.fetchone():
+        abort(404)
+    # TODO:
+    if not request.json:
+        abort(400)
+    cur = g.db.cursor()
+    cur.execute('UPDATE variables SET name = %s, validated = %s, min = %s, max = %s WHERE id = %s;', 
+            (request.json['name'], request.json['validated'], request.json['min'], request.json['max'], variable_id,))
+    cur.close()
+    g.db.commit()
+    return jsonify(), 200
 
 @app.route('/api/variables/<int:variable_id>', methods=['DELETE'])
 def delete_variable(variable_id):
-    for variable in variables:
-        if variable['id'] == variable_id:
-            variables.remove(variable['id'])
-            return jsonify(), 200
-    abort(404)
+    cur = g.db.cursor()
+    cur.execute('SELECT id FROM variables WHERE id = %s LIMIT 1;', (variable_id,))
+    if not cur.fetchone():
+        abort(404)
+    cur.execute('DELETE FROM variables WHERE id = %s;', (variable_id,))
+    cur.close()
+    g.db.commit()
+    return jsonify(), 200
 
 @app.route('/api/tasks', methods=['POST'])
 def create_task():
-    summ = 0
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute('SELECT id, min, max FROM variables;')
+    variables = cur.fetchall()
+    cur.close()
     for pair in request.json['inputs']:
-        summ += pair['value']
-    return jsonify({'output': summ})
+        valid = False
+        for variable in variables:
+            if pair['variable'] == variable['id']:
+                if pair['value'] >= variable['min'] and pair['value'] <= variable['max']:
+                    valid = True
+                    break
+        if not valid:
+            abort(400)
+    valid = False
+    for variable in variables:
+        if request.json['output'] == variable['id']:
+            valid = True
+            break
+    if not valid:
+        abort(400)
+    return jsonify({'output': 43})
 
 app.run(debug=True)
