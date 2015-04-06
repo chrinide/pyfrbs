6148c5bc731c043dba9aa8ffd4f33868cfe1c9b6
Ivan Zenin <i.zenin@gmx.com>
2015-04-07 02:17:22 +0300

    Change default database credentials

--- expert_run.py
+++ expert_run.py
@@ -10,9 +10,9 @@ from PyQt5.QtWidgets import QApplication
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, add_help=False)
     parser.add_argument('-a', dest='address', default='127.0.0.1:5432')
-    parser.add_argument('-d', dest='database', default='fuzzy')
-    parser.add_argument('-u', dest='username', default='user1')
-    parser.add_argument('-p', dest='password', default='pass1')
+    parser.add_argument('-d', dest='database', default='db')
+    parser.add_argument('-u', dest='username', default='user')
+    parser.add_argument('-p', dest='password', default='pass')
     opts = parser.parse_args()
     app = QApplication(sys.argv)
     window = Window(host=opts.address.split(':')[0], port=opts.address.split(':')[1], 
