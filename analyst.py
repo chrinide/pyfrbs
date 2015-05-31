32168e3438b96ccd787e62effafec8971cacdfeb
Ivan Zenin <i.zenin@gmx.com>
2015-05-31 19:57:29 +0300

    Redesign of the analyst interface

--- analyst.py
+++ analyst.py
@@ -1,46 +1,44 @@
+#!/usr/bin/env python
+
+import urllib3
 import json
 
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QDoubleValidator
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QListWidgetItem
 from PyQt5.QtWidgets import QMainWindow
-from PyQt5.QtWidgets import QTableWidgetItem
-from PyQt5.QtWidgets import QTreeWidgetItem
 from PyQt5.uic import loadUi
-#import psycopg2
-import urllib3
-
 
 class Window(QMainWindow):
-    def __init__(self, *args,host,port):  
+    def __init__(self, *args, host, port):  
         super(Window, self).__init__(*args)
 
-       
-        loadUi('fuzzy.ui', self)
+        loadUi('analyst.ui', self)
         
+        self.conn = urllib3.connection_from_url('http://%s:%s/' % (host, port))
+
         #Initialize
         
-        self.fillComboVar(self.comboBoxInVar, 'variables')
-        self.comboBoxInVar.currentIndexChanged.connect(self.onVariableSelected)
-        
-    def fillComboVar(self,combo,table):
+        self.fillCombo(self.uiInputCombo)
+        self.fillCombo(self.uiOutputCombo)
+#        self.comboBoxInVar.currentIndexChanged.connect(self.onVariableSelected)
+
+    def fillCombo(self, combo):
         combo.clear()
-        conn = urllib3.connection_from_url('http://127.0.0.1:5000/')
-        r = conn.request('GET', '/api/variables')
+        r = self.conn.request('GET', '/api/variables')
         data = json.loads(r.data.decode('utf-8'))
-        for variable in data ['variables']:
-            combo.addItem(variable['name'],variable['id'])
+        for variable in data['variables']:
+            combo.addItem(variable['name'], variable['id'])
         combo.setCurrentIndex(-1)
      
-    def onVariableSelected(self):
-        if (self.comboBoxInVar.isEditable() == True):
-            return  
-         
-        self.comboBoxOutVar.setCurrentIndex(-1)
-        self.pushButtonAddInVar.setEnabled(True)
-        self.pushButtonSend.setEnabled(False)
-        self.lineEditMax.clear()
-        self.lineEditMin.clear()
+#    def onVariableSelected(self):
+#        if (self.comboBoxInVar.isEditable() == True):
+#            return  
+#         
+#        self.comboBoxOutVar.setCurrentIndex(-1)
+#        self.pushButtonAddInVar.setEnabled(True)
+#        self.pushButtonSend.setEnabled(False)
+#        self.lineEditMax.clear()
+#        self.lineEditMin.clear()
         #self.listViewInVar.clear()
         
--- analyst_run.py
+++ analyst_run.py
@@ -1,4 +1,6 @@
-from fuzzy import Window
+#!/usr/bin/env python
+
+from analyst import Window
 
 import sys
 import argparse
@@ -7,7 +9,7 @@ from PyQt5.QtWidgets import QApplication
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, add_help=False)
-    parser.add_argument('-a', dest='address', default='127.0.0.1:5432')
+    parser.add_argument('-a', dest='address', default='127.0.0.1:5000')
     opts = parser.parse_args()
     app = QApplication(sys.argv)
     window = Window(host=opts.address.split(':')[0], port=opts.address.split(':')[1])
