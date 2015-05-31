e2bfa8cea5b78ee9a96374ee09b59c09b828a646
Ivan Zenin <i.zenin@gmx.com>
2015-05-31 18:45:59 +0300

    Add analyst interface files

---
+++ analyst_run.py
@@ -0,0 +1,15 @@
+from fuzzy import Window
+
+import sys
+import argparse
+
+from PyQt5.QtWidgets import QApplication
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter, add_help=False)
+    parser.add_argument('-a', dest='address', default='127.0.0.1:5432')
+    opts = parser.parse_args()
+    app = QApplication(sys.argv)
+    window = Window(host=opts.address.split(':')[0], port=opts.address.split(':')[1])
+    window.show()
+    sys.exit(app.exec_())
